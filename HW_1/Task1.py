4f0e2c95015d8f59fa89f90f7250c34e6911775d
Anastasia <anastasia.koles@gmail.com>
2023-08-17 17:16:10 +0300

    the task for HW 1 was completed

---
+++ HW_1/Task1.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route('/cap/')
+def cap():
+
+    return render_template("Cap.html")
+
+@app.route("/about/")
+def about():
+
+
+    return render_template("about.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
